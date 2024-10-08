aa207765e9a9b336d83bc30f66f6cc923d371a94
Arifahanggraini05 <arifahanggraini05@gmail.com>
2024-10-08 09:57:27 +0700

    Update PBO_Polimorfisme_Pert5.js

--- PBO_Polimorfisme_Pert5.js
+++ PBO_Polimorfisme_Pert5.js
@@ -18,11 +18,19 @@ class Programmer extends People {
     }
 }
 
+class DataAnalyst extends People {
+    tampilkanProfession() {
+        console.log("Peran: Data Analis");
+    }
+}
+
 // Menggunakan polimorfisme
 const orang1 = new People();
 const mahasiswa1 = new DBE();
 const dosen1 = new Programmer();
+const DA1 = new DataAnalyst();
 
 orang1.tampilkanProfession();    // Output: Peran: Orang biasa
 mahasiswa1.tampilkanProfession(); // Output: Peran: Database Engineer
 dosen1.tampilkanProfession();     // Output: Peran: Programmer
+DA1.tampilkanProfession(); // Output : Peran : Data Analis
