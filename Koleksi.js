2d27ebc59babd3b7f76ff8db4aa4f2c8754bd961
Arifahanggraini05 <arifahanggraini05@gmail.com>
2024-10-29 14:12:28 +0700

    Update Koleksi.js

--- Koleksi.js
+++ Koleksi.js
@@ -1,11 +1,10 @@
 // Menggunakan List (Array)
 let listMahasiswa = ["Budi", "Siti", "Agus", "Arifah", "Sza"];
 listMahasiswa.push("Ani"); // Menambahkan elemen baru
-console.log(listMahasiswa); // Output: ["Budi", "Siti", "Agus", "Ani"]
 console.log(listMahasiswa[2]); //  untuk mengakses elemen tertentu
-listMahasiswa[5] = "Anggraini"; // untuk update atau mengubah elemen tertentu
-console.log(listMahasiswa); 
+listMahasiswa[3] = "Anggraini"; // untuk update atau mengubah elemen tertentu
 listMahasiswa.pop(); // untuk menghapus elemen terakhir dalam list
+listMahasiswa.splice(1,1); // untuk menghapus elemen tertentu
 console.log(listMahasiswa);
 
 
@@ -14,8 +13,7 @@ let setMahasiswa = new Set(["Budi", "Siti", "Agus"]);
 setMahasiswa.add("Ani");
 setMahasiswa.add("Budi"); // Tidak akan menambahkan karena Budi sudah ada
 setMahasiswa.delete("Siti"); 
-setMahasiswa[0] = "Bima";
-console.log(setMahasiswa); // Output: Set(4) {"Budi", "Siti", "Agus", "Ani"}
+console.log(setMahasiswa);
 
 // Menggunakan Map
 let mapNilai = new Map();
@@ -23,4 +21,6 @@ mapNilai.set("Budi", 90);
 mapNilai.set("Siti", 85);
 mapNilai.set("Agus", 95);
 console.log(mapNilai.get("Budi")); // Output: 90
-console.log(mapNilai.delete("Budi"));
+mapNilai.set("Agus", 90); // untuk update nilai tertentu
+console.log(mapNilai.delete("Budi")); // untuk menghapus elemen tertentu
+console.log(mapNilai);
